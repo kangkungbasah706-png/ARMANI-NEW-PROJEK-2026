b85caa28b385812a6ca1dfb0cf41f799da1b76e3
kangkungbasah706-png <kangkungbasah706@gmail.com>
2026-01-22 12:12:10 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -303,9 +303,6 @@ const App: React.FC = () => {
 
                 {/* GAMBAR PRODUK BOX - BERAKHIR RAPI (CLEAN ROUNDED) */}
                 <div className="flex-grow bg-[#ff7373] rounded-[24px] overflow-hidden shadow-xl flex flex-col min-h-0 relative border border-white/20">
-                   <div className="w-full py-2.5 bg-black/5 text-center border-b border-black/5 shrink-0">
-                      <span className="text-[12px] font-black text-black tracking-[0.3em] uppercase italic truncate px-2 block">{product.name}</span>
-                   </div>
                    <div className="flex-grow relative w-full overflow-hidden bg-black rounded-b-[24px] min-h-[420px]">
                       <img src={product.imageUrl} alt={product.name} className="w-full h-full object-cover opacity-90" />
                       <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-transparent to-transparent"></div>
