e42aaaa45b49d5ed840dfa6f3b2e2b80d028c479
kangkungbasah706-png <kangkungbasah706@gmail.com>
2026-01-22 11:36:15 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -390,7 +390,7 @@ const App: React.FC = () => {
 
                    {/* FITUR WAKTU TUGAS - MENGISI KOTAK KOSONG */}
                    <div className="mt-auto pt-4 border-t border-black/5 flex flex-col items-center justify-center shrink-0">
-                      <div className="bg-black/[0.03] rounded-2xl px-8 py-3 border border-black/5 flex flex-col items-center shadow-inner relative overflow-hidden group">
+                      <div className="bg-black/[0.03] rounded-2xl px-8 py-1.5 border border-black/5 flex flex-col items-center shadow-inner relative overflow-hidden group">
                          <div className="absolute top-0 left-0 w-full h-[2px] bg-gradient-to-r from-transparent via-[#d13a3a]/20 to-transparent"></div>
                          <div className="flex items-center gap-2.5 mb-1.5">
                             <svg className="w-4 h-4 text-black/40" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
